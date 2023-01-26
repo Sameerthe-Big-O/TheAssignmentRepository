7da0a27beff84daa48f82c869f4ad86f1068e0cd
Samtheflexboy <110691583+Samtheflexboy@users.noreply.github.com>
2023-01-26 12:46:41 -0800

    Update typecorrection.js

---
+++ typecorrection.js
@@ -0,0 +1,2 @@
+//type correction is basically occurs when javascript language try to convert the one type into another (implicity)  for example if we say  4==='4' now they both 
+//are not equal but js will convert the string 4 into the 4 number which's integer
